8e03f260a81f32b99c7fdfecbcc39c670611d607
Niklas Engvall <engvallen@gmail.com>
2021-04-09 18:06:17 +0200

    update for gnome 40.0

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # spotkeys
+
+Gnome-shell extension that adds buttons for spotify player to the panel.
+
+### Changelog
+* v.2
+  - Support Gnome 40.0
+* v.1
+  - First version
+
+### Settings
+
+To move buttons, replace ```_rightBox``` by ```_leftBox``` or ```_centerBox```, to change global position.
+~/.local/share/gnome-shell/extensions/spotkeys@snhy/extension.js
+```
+function enable() {
+  Main.panel._centerBox.insert_child_at_index(buttonNext, 1);
+  Main.panel._centerBox.insert_child_at_index(buttonPause, 1);
+  Main.panel._centerBox.insert_child_at_index(buttonPrev, 1);
+}
+```
--- extension.js
+++ extension.js
@@ -6,33 +6,21 @@ const iconPrev = new St.Icon({
   icon_name: 'media-skip-backward-symbolic',
   style_class: 'system-status-icon'
 });
-
-const iconPlay = new St.Icon({
-  icon_name: 'media-playback-start-symbolic',
-  style_class: 'system-status-icon'
-});
-
 const iconPause = new St.Icon({
-  icon_name: 'media-playback-stop-symbolic',
+  icon_name: 'media-playback-pause-symbolic',
   style_class: 'system-status-icon'
 });
-
 const iconNext = new St.Icon({
   icon_name: 'media-skip-forward-symbolic',
   style_class: 'system-status-icon'
 });
 
 let buttonPrev = new St.Button({ style_class: 'panel-button'});
-let buttonPlay = new St.Button({ style_class: 'panel-button'});
 let buttonPause = new St.Button({ style_class: 'panel-button'});
 let buttonNext = new St.Button({ style_class: 'panel-button'});
 
-function _play() {
-  Util.spawn(['/usr/bin/dbus-send', '--print-reply', '--dest=org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2', 'org.mpris.MediaPlayer2.Player.Play']);
-}
-
 function _pause() {
-  Util.spawn(['/usr/bin/dbus-send', '--print-reply', '--dest=org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2', 'org.mpris.MediaPlayer2.Player.Pause']);
+  Util.spawn(['/usr/bin/dbus-send', '--print-reply', '--dest=org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2', 'org.mpris.MediaPlayer2.Player.PlayPause']);
 }
 
 function _prev() {
@@ -50,23 +38,18 @@ function init() {
   buttonPause.set_child(iconPause);
   buttonPause.connect('button-press-event', _pause);
 
-  buttonPlay.set_child(iconPlay);
-  buttonPlay.connect('button-press-event', _play);
-
   buttonNext.set_child(iconNext);
   buttonNext.connect('button-press-event', _next);
 }
 
 function enable() {
-  Main.panel._centerBox.insert_child_at_index(buttonNext, 0);
-  Main.panel._centerBox.insert_child_at_index(buttonPlay, 0);
-  Main.panel._centerBox.insert_child_at_index(buttonPause, 0);
-  Main.panel._centerBox.insert_child_at_index(buttonPrev, 0);
+  Main.panel._centerBox.insert_child_at_index(buttonNext, 1);
+  Main.panel._centerBox.insert_child_at_index(buttonPause, 1);
+  Main.panel._centerBox.insert_child_at_index(buttonPrev, 1);
 }
 
 function disable() {
   Main.panel._centerBox.remove_child(buttonPrev);
   Main.panel._centerBox.remove_child(buttonPause);
-  Main.panel._centerBox.remove_child(buttonPlay);
   Main.panel._centerBox.remove_child(buttonNext);
 }
